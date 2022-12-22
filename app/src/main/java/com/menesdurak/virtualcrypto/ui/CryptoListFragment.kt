00ebeb1db06cd0b2363a560eb343006658270821
menesdurak <m.enes.durak@gmail.com>
2022-12-22 21:28:14 +0300

    CryptoListFragment created

---
+++ app/src/main/java/com/menesdurak/virtualcrypto/ui/CryptoListFragment.kt
@@ -0,0 +1,30 @@
+package com.menesdurak.virtualcrypto.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.menesdurak.virtualcrypto.R
+import com.menesdurak.virtualcrypto.databinding.FragmentCryptoListBinding
+
+class CryptoListFragment : Fragment() {
+
+    private var _binding: FragmentCryptoListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCryptoListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
