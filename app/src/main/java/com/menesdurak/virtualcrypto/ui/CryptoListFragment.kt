7713bdaea5342989c06131890a4a64bff9c9eec5
menesdurak <m.enes.durak@gmail.com>
2022-12-22 21:56:32 +0300

    CryptoListFragment added

--- app/src/main/java/com/menesdurak/virtualcrypto/ui/CryptoListFragment.kt
+++ app/src/main/java/com/menesdurak/virtualcrypto/ui/CryptoListFragment.kt
@@ -5,13 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
 import com.menesdurak.virtualcrypto.R
+import com.menesdurak.virtualcrypto.data.remote.RetrofitClient
+import com.menesdurak.virtualcrypto.data.remote.api.CryptoApi
+import com.menesdurak.virtualcrypto.data.repository.CryptoRepository
 import com.menesdurak.virtualcrypto.databinding.FragmentCryptoListBinding
+import retrofit2.Retrofit
 
 class CryptoListFragment : Fragment() {
 
     private var _binding: FragmentCryptoListBinding? = null
     private val binding get() = _binding!!
+
+    private val retrofit: Retrofit by lazy {
+        RetrofitClient.getInstance()
+    }
+    private val cryptoApi: CryptoApi by lazy {
+        retrofit.create(CryptoApi::class.java)
+    }
+    private val cryptoRepository: CryptoRepository by lazy {
+        CryptoRepository(cryptoApi)
+    }
+    private val cryptoViewModel: CryptoViewModel by viewModels {
+        ViewModelFactory(cryptoRepository)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
