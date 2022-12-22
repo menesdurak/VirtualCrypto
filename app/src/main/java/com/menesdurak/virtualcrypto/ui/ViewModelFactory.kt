587ff92f73ea657f23903fea1cd873556ecfd11c
menesdurak <m.enes.durak@gmail.com>
2022-12-22 20:12:51 +0300

    Added viewmodelfactory

---
+++ app/src/main/java/com/menesdurak/virtualcrypto/ui/ViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.menesdurak.virtualcrypto.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.menesdurak.virtualcrypto.data.repository.CryptoRepository
+
+class ViewModelFactory(
+    private val repository: CryptoRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(CryptoViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return CryptoViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
