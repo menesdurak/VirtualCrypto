37e8078caed8f7feab3fa6d6f6539a90f19d3aec
menesdurak <m.enes.durak@gmail.com>
2022-12-23 18:36:06 +0300

    Changed background color of even number of elements of recyclerview

--- app/src/main/java/com/menesdurak/virtualcrypto/ui/list/CryptoListAdapter.kt
+++ app/src/main/java/com/menesdurak/virtualcrypto/ui/list/CryptoListAdapter.kt
@@ -1,25 +1,38 @@
 package com.menesdurak.virtualcrypto.ui.list
 
+import android.graphics.Color
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.menesdurak.virtualcrypto.R
 import com.menesdurak.virtualcrypto.model.Usd
 
-class CryptoListAdapter(private val cryptoList: List<Usd>): RecyclerView.Adapter<CryptoListAdapter.CryptoViewHolder>() {
-    class CryptoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+class CryptoListAdapter(private val cryptoList: List<Usd>) :
+    RecyclerView.Adapter<CryptoListAdapter.CryptoViewHolder>() {
+    class CryptoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CryptoViewHolder {
         return CryptoViewHolder(
             LayoutInflater.from(parent.context)
-                .inflate(R.layout.row_crypto_list, parent, false))
+                .inflate(R.layout.row_crypto_list, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: CryptoViewHolder, position: Int) {
-        holder.itemView.findViewById<TextView>(R.id.rowSymbol).text = cryptoList[position].FROMSYMBOL
-        holder.itemView.findViewById<TextView>(R.id.rowPrice).text = cryptoList[position].PRICE.toString()
+        holder.itemView.findViewById<TextView>(R.id.rowSymbol).text =
+            cryptoList[position].FROMSYMBOL
+        holder.itemView.findViewById<TextView>(R.id.rowPrice).text =
+            cryptoList[position].PRICE.toString()
+
+        //Even number of layout elements background set to gray
+        if (position % 2 == 0) {
+            holder.itemView.findViewById<LinearLayout>(R.id.rowLayout)
+                .setBackgroundColor(Color.parseColor("#f0f0f0"))
+        }
     }
 
     override fun getItemCount(): Int = cryptoList.size
