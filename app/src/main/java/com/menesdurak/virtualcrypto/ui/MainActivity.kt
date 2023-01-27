971026f6036bda81ed7c6459885897ec3d9f86d7
menesdurak <m.enes.durak@gmail.com>
2023-01-27 11:22:36 +0300

    Removed unnecessary codes

--- app/src/main/java/com/menesdurak/virtualcrypto/ui/MainActivity.kt
+++ app/src/main/java/com/menesdurak/virtualcrypto/ui/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.menesdurak.virtualcrypto.ui
 
+import android.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.viewModels
 import com.menesdurak.virtualcrypto.data.remote.RetrofitClient
@@ -13,20 +15,6 @@ import com.menesdurak.virtualcrypto.ui.viewmodel.ViewModelFactory
 import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
-
-//    private val retrofit: Retrofit by lazy {
-//        RetrofitClient.getInstance()
-//    }
-//    private val cryptoApi: CryptoApi by lazy {
-//        retrofit.create(CryptoApi::class.java)
-//    }
-//    private val cryptoRepository: CryptoRepository by lazy {
-//        CryptoRepository(cryptoApi)
-//    }
-//    private val cryptoViewModel: CryptoViewModel by viewModels {
-//        ViewModelFactory(cryptoRepository)
-//    }
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,17 +23,4 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
     }
-
-//    override fun onStart() {
-//        super.onStart()
-//
-//        cryptoViewModel.getAllCryptos()
-//
-//        cryptoViewModel.cryptoList.observe(this) {
-//            Toast.makeText(this, it.RAW.BTC.USD.PRICE.toString(), Toast.LENGTH_SHORT).show()
-//            Toast.makeText(this, it.RAW.ETH.USD.PRICE.toString(), Toast.LENGTH_SHORT).show()
-//            println(it.RAW.BTC.USD.PRICE.toString())
-//            println(it.RAW.ETH.USD.PRICE.toString())
-//        }
-//    }
 }
